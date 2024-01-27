11f2bf572cf1bea0acf5299ef9f3cf20c3436a84
NafisaKaruri <nafeesahassan016@gmail.com>
2024-01-26 18:37:23 -0800

    task 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - infinite while
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - creates five zombies
+ * Return: 0
+ */
+int main(void)
+{
+	pid_t pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %i\n", pid);
+			sleep(1);
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+	return (0);
+}
