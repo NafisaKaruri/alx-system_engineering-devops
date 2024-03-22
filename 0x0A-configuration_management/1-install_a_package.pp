55862ae09643483f5aba3cd7b2f7de1b1156f3b5
NafisaKaruri <nafeesahassan016@gmail.com>
2024-03-22 08:51:02 -0700

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
+#!/usr/bin/pup
 # Installing flask from pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # create a manifext that kills a process killmenow
 exec { 'pkill':
-  command => 'pkill -9 -f killmenow',
-  path    => {'/usr/bin', '/usr/sbin', '/bin'}
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
