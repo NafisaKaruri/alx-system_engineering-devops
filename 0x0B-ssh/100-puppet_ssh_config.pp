d8e441b765a2c085c9c797d6fa0d105bffb9440e
NafisaKaruri <nafeesahassan016@gmail.com>
2024-03-24 11:20:32 -0700

    advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# change ssh configuration using puppet
+file_line { 'No password authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'The identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
