938d2c56fe6d54ebede5905f7203c4f31819a2af
NafisaKaruri <nafeesahassan016@gmail.com>
2024-08-15 13:27:06 +0000

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API
+    Returns: the number of subscribers for a given subreddit
+             0 if not a valid subreddit
+    """
+    r = requests.get(
+        "https://www.reddit.com//r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if r.status_code == 200:
+        return r.json().get("data").get("subscribers")
+    else:
+        return 0;
