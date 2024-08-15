f9db68fddc52f65eaa650bce476a7e2cb6818f30
NafisaKaruri <nafeesahassan016@gmail.com>
2024-08-15 22:55:47 +0000

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -15,15 +15,15 @@ def recurse(subreddit, hot_list[]):
     r = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={"User-Agent": "Custom"},
-        params={"after": after, "count": count, "limit": 100},
+        params={"after": after},
     )
 
     if r.status_code == 200:
-        d = r.json().get("data")
-        after = d.get("after"):
-        count += d.get("dist")
-        for child in d.get("children"):
-            hot_list.append(child.get("data").get("title"))
+        for child in r.json().get("data").get("children"):
+            d = child.get("data")
+            title = d.get("title")
+            hot_list.append(title)
+        after = r.json().get("data").get("after")
         if after is not None:
             return recurse(subreddit, hot_list)
         return hot_list
