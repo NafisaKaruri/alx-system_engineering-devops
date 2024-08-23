8c3e58b42640cdded4d92ef9f60f86f46fdcb101
NafisaKaruri <nafeesahassan016@gmail.com>
2024-08-23 00:10:26 +0000

    updated 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # increase holberton user limits
 
-exec { 'increase hard file limit 4 -> 50000':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+exec { 'increase hard file limit 5 -> 50000':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'increase soft file limit 5 -> 50000':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+exec { 'increase soft file limit 4 -> 50000':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
