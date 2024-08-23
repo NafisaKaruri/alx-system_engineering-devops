4b9abe5ff7432ae35fc917c5f1dd8365b9cd5a27
NafisaKaruri <nafeesahassan016@gmail.com>
2024-08-23 00:12:34 +0000

    puppet-lint err

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # change request limit from 15 to 4096 in /etc/default/nginx
 
-exec {"modify limit":
+exec {'modify limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
